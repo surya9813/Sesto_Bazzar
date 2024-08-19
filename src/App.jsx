42b267999943427ccb513621534ff1b81d5182ac
surya9813 <suryanarayan2056@gmail.com>
2024-08-19 10:09:48 +0545

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
+  // const Styling = {
+  //   color :"green",
+  //   font:"4rem"
+  // }
   return (
     <>
-    <h1>Hello World</h1>
+      {/* <h1 style={{color:"green"}}>Hello World</h1> */}
+      {/* <h1 style={Styling}>Hello World</h1> */}
+      {/* <h1 className="hello">Hello World</h1> */}
+      <h1 className="text-red-500 text-center text-2xl">Hello World</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
